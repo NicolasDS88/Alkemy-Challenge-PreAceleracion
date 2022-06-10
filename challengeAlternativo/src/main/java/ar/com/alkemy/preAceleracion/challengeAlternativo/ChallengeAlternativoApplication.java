c62d8a12b06cf45f8b6d049b5b3c9ea47bc6fba1
Nicolas da Silva <nicolas.dasilva88@gmail.com>
2022-06-10 11:47:44 -0300

    We can Star now the alternative challenge

---
+++ challengeAlternativo/src/main/java/ar/com/alkemy/preAceleracion/challengeAlternativo/ChallengeAlternativoApplication.java
@@ -0,0 +1,13 @@
+package ar.com.alkemy.preAceleracion.challengeAlternativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAlternativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAlternativoApplication.class, args);
+	}
+
+}
---
+++ challengeAlternativo/src/test/java/ar/com/alkemy/preAceleracion/challengeAlternativo/ChallengeAlternativoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.alkemy.preAceleracion.challengeAlternativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAlternativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
